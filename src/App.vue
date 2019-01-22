41faacf43f2ae249801251cece107d4baab160e5
Filippo Conti <filippo@codekraft.it>
2019-01-22 17:41:34 +0100

    Updated demo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="container">
+  <div id="app">
     <nav class="navbar">
       <div class="navbar-brand">
         <img src="./assets/logo.png" alt="VueJs" />
@@ -11,77 +11,78 @@
         </h2>
       </div>
     </nav>
+    <main>
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-half has-text-right">
+              <h2 class="is-size-4">Best match</h2>
+              <p class="is-size-6 has-text-grey-light">It will find the best match automatically</p>
+              <div class="field has-addons has-addons-right">
+                <p class="control">
+                  <input class="input" type="text" v-model="units" placeholder="Conversion units">
+                </p>
+                <p class="control">
+                  <span class="select">
+                    <select v-model="unitsFrom">
+                      <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
+                    </select>
+                  </span>
+                </p>
+              </div>
 
-    <section class="section main">
-      <div class="container">
-        <div class="columns">
-          <div class="column is-half has-text-right">
-            <h2 class="is-size-4">Best match</h2>
-            <p class="is-size-6 has-text-grey-light">It will find the best match automatically</p>
-            <div class="field has-addons has-addons-right">
-              <p class="control">
-                <input class="input" type="text" v-model="units" placeholder="Conversion units">
-              </p>
-              <p class="control">
-                <span class="select">
-                  <select v-model="unitsFrom">
-                    <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
-                  </select>
-                </span>
-              </p>
+              <p>{{units | convert(unitsFrom, 'best')}}</p>
             </div>
 
-            <p>{{units | convert(unitsFrom, 'best')}}</p>
-          </div>
-
-          <div class="column is-half">
-            <pre v-text="unitsCode"></pre>
-            <pre v-text="unitsRaw"></pre>
+            <div class="column is-half">
+              <pre v-text="unitsCode"></pre>
+              <pre v-text="unitsRaw"></pre>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-half has-text-right">
+              <h2 class="is-size-4">Manual Conversion</h2>
+              <p class="is-size-6 has-text-grey-light">You choose what format to convert</p>
+              <div class="field has-addons has-addons-right">
+                <p class="control">
+                  <input class="input" type="text" v-model="manualUnits" placeholder="Enter a number">
+                </p>
+                <p class="control">
+                  <span class="select">
+                    <select v-model="manualUnitsFrom">
+                      <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
+                    </select>
+                  </span>
+                </p>
+              </div>
 
-      <div class="container">
-        <div class="columns">
-          <div class="column is-half has-text-right">
-            <h2 class="is-size-4">Manual Conversion</h2>
-            <p class="is-size-6 has-text-grey-light">You choose what format to convert</p>
-            <div class="field has-addons has-addons-right">
-              <p class="control">
-                <input class="input" type="text" v-model="manualUnits" placeholder="Enter a number">
-              </p>
-              <p class="control">
-                <span class="select">
-                  <select v-model="manualUnitsFrom">
-                    <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
-                  </select>
-                </span>
-              </p>
+              <div class="field has-addons has-addons-right">
+                <p class="control">
+                  <input class="input" type="text" v-model="manualSuffix" placeholder="Conversion suffix">
+                </p>
+                <p class="control">
+                  <span class="select">
+                    <select v-model="manualUnitsTo">
+                      <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
+                    </select>
+                  </span>
+                </p>
+              </div>
+
             </div>
 
-            <div class="field has-addons has-addons-right">
-              <p class="control">
-                <input class="input" type="text" v-model="manualSuffix" placeholder="Conversion suffix">
-              </p>
-              <p class="control">
-                <span class="select">
-                  <select v-model="manualUnitsTo">
-                    <option v-for="(possibility, index) in possibilities" :key="index" :value="possibility">{{possibility}}</option>
-                  </select>
-                </span>
-              </p>
+            <div class="column is-half">
+              <pre v-text="manualCode"></pre>
+              <pre>{{ manualUnits | convert(manualUnitsFrom, manualUnitsTo, manualSuffix) }}</pre>
             </div>
-
-          </div>
-
-          <div class="column is-half">
-            <pre v-text="manualCode"></pre>
-            <pre>{{ manualUnits | convert(manualUnitsFrom, manualUnitsTo, manualSuffix) }}</pre>
           </div>
         </div>
-      </div>
-    </section>
-
+      </section>
+    </main>
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
@@ -124,6 +125,32 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+#app .field {
+  margin: .75rem;
+}
+
+#app pre {
+  margin-bottom: 10px;
+}
+
+#app main {
+  flex-grow: 1;
+}
+
 .navbar {
   padding: 20px;
   display: flex;
@@ -132,16 +159,6 @@ export default {
 .navbar-brand img {
   width: 64px;
   height: 64px;
-  margin-right: 20px;
-}
-.section.main > .container {
-  max-width: 800px;
-  margin-bottom: 25px;
-}
-.section.main .field {
-  margin: .75rem;
-}
-.section.main pre {
-  margin-bottom: 10px;
+  margin-right: 16px;
 }
 </style>
